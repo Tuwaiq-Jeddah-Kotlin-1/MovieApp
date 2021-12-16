6f5eac4773bbcefb0151d14c2b7a46366407068b
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-12-16 15:52:33 +0300

    make logo for my app , and change all design and layout .

--- app/src/main/java/com/tuwaiq/movieapp/SplashScreenActivity.kt
+++ app/src/main/java/com/tuwaiq/movieapp/SplashScreenActivity.kt
@@ -10,14 +10,10 @@ class SplashScreenActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        window.setFlags(
-            WindowManager.LayoutParams.FLAG_FULLSCREEN,
-            WindowManager.LayoutParams.FLAG_FULLSCREEN
-        )
         Handler().postDelayed({
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
-        }, 2000)
+        }, 4500)
     }
 }
---
+++ app/src/main/java/com/tuwaiq/movieapp/data/remot/Movie.kt
@@ -0,0 +1,15 @@
+package com.tuwaiq.movieapp.data.remot
+
+import android.os.Parcelable
+import kotlinx.parcelize.Parcelize
+
+
+@Parcelize
+data class Movie(
+    val id: String,
+    val overview: String?,
+    val poster_path: String,
+    val original_title: String,
+):Parcelable {
+    val baseUrl get() = "https://image.tmdb.org/t/p/w500"
+}
