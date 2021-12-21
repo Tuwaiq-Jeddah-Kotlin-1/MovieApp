086e7428a8119dc4662ed4f09ca81acdf2a1095d
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-12-21 16:45:14 +0300

    make button to refresh page and retrieve the data

--- app/src/main/java/com/tuwaiq/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/tuwaiq/movieapp/ui/movie/MovieFragment.kt
@@ -2,8 +2,10 @@ package com.tuwaiq.movieapp.ui.movie
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import androidx.paging.LoadState
 import com.tuwaiq.movieapp.R
 import com.tuwaiq.movieapp.data.remot.Movie
 import com.tuwaiq.movieapp.databinding.FragmentMovieBinding
@@ -23,10 +25,29 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
         binding.apply {
             rvMovie.setHasFixedSize(true)
             rvMovie.adapter = adapter.withLoadStateHeaderAndFooter(
-                header = MovieLoadStateAdapter { adapter.retry() },
-                footer = MovieLoadStateAdapter { adapter.retry() }
-
+                header = MovieLoadStateAdapter(adapter::retry),
+                footer = MovieLoadStateAdapter(adapter::retry)
             )
+            btnTryAgain.setOnClickListener {
+                adapter.retry()
+            }
+        }
+        adapter.addLoadStateListener { loadState ->
+            binding.apply {
+                progressBar.isVisible = loadState.source.refresh is LoadState.Loading
+                rvMovie.isVisible = loadState.source.refresh is LoadState.NotLoading
+                btnTryAgain.isVisible = loadState.source.refresh is LoadState.Error
+                tvFailed.isVisible = loadState.source.refresh is LoadState.Error
+
+                if (loadState.source.refresh is LoadState.NotLoading && loadState.append.endOfPaginationReached && adapter.itemCount < 1) {
+                    rvMovie.isVisible = false
+                    tvNotFound.isVisible = true
+                } else {
+                    tvNotFound.isVisible = false
+                }
+            }
+
+
         }
         viewModel.movies.observe(viewLifecycleOwner) {
             adapter.submitData(viewLifecycleOwner.lifecycle, it)
