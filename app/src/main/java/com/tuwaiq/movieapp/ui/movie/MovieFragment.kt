17b4fcd3e2a5005725065bb38a9f11ef09e86dcb
faisalha98 <fisal.alharthi.jed.k1@tuwaiq.edu.sa>
2021-12-21 14:15:36 +0300

    Make class MovieLoadStateAdapter If there is no active load operation and no error , Indicates the PagingData is not currently loading, and no error currently observed.

--- app/src/main/java/com/tuwaiq/movieapp/ui/movie/MovieFragment.kt
+++ app/src/main/java/com/tuwaiq/movieapp/ui/movie/MovieFragment.kt
@@ -22,10 +22,14 @@ class MovieFragment : Fragment(R.layout.fragment_movie) {
         val adapter = MovieAdapter()
         binding.apply {
             rvMovie.setHasFixedSize(true)
-            rvMovie.adapter = adapter
+            rvMovie.adapter = adapter.withLoadStateHeaderAndFooter(
+                header = MovieLoadStateAdapter { adapter.retry() },
+                footer = MovieLoadStateAdapter { adapter.retry() }
+
+            )
         }
         viewModel.movies.observe(viewLifecycleOwner) {
-            adapter.submitData(viewLifecycleOwner.lifecycle,it)
+            adapter.submitData(viewLifecycleOwner.lifecycle, it)
         }
     }
 
